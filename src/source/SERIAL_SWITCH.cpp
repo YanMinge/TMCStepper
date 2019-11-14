2128748dde3b7ed851e6847bc65eca3f96c1110c
Mark Yan <myan@sierrawireless.com>
2019-11-14 19:14:12 +0800

    add time delay for test

--- src/source/SERIAL_SWITCH.cpp
+++ src/source/SERIAL_SWITCH.cpp
@@ -4,13 +4,13 @@ SSwitch::SSwitch( const uint16_t pin, const uint8_t address) :
   p1(pin),
   addr(address)
   {
-    pinMode(pin, OUTPUT);
+    //pinMode(pin, OUTPUT);
   }
 
 void SSwitch::active() {
   deactivate_all();
-  delay(1);
   digitalWrite(p1, addr & 0b01 ? HIGH : LOW);
+  delayMicroseconds(50);
 }
 
 void SSwitch::deactivate_all() {
