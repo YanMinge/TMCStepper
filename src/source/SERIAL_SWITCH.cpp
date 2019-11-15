897487402c80445597672a9b895fd32e72f811ca
Mark Yan <myan@sierrawireless.com>
2019-11-15 16:01:06 +0800

    Reverse TMC serial port enable signal

--- src/source/SERIAL_SWITCH.cpp
+++ src/source/SERIAL_SWITCH.cpp
@@ -4,18 +4,18 @@ SSwitch::SSwitch( const uint16_t pin, const uint8_t address) :
   p1(pin),
   addr(address)
   {
-    //pinMode(pin, OUTPUT);
+    pinMode(pin, OUTPUT);
   }
 
 void SSwitch::active() {
   deactivate_all();
-  digitalWrite(p1, addr & 0b01 ? HIGH : LOW);
+  digitalWrite(p1, addr & 0b01 ? LOW : HIGH);
   delayMicroseconds(50);
 }
 
 void SSwitch::deactivate_all() {
-    digitalWrite(P0_06, HIGH);
-    digitalWrite(P0_07, HIGH);
-    digitalWrite(P0_08, HIGH);
-    digitalWrite(P0_09, HIGH);
+  digitalWrite(P0_06, LOW);
+  digitalWrite(P0_07, LOW);
+  digitalWrite(P0_08, LOW);
+  digitalWrite(P0_09, LOW);
 }
